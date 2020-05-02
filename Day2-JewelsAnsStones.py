53d2a94159b5140c26066ca243eb97deb0d82577
akhilbommu <akhilbommu14399@gmail.com>
2020-05-02 15:16:31 +0530

    Day2 Solution

---
+++ Day2-JewelsAnsStones.py
@@ -0,0 +1,29 @@
+"""
+Problem Link: "https://leetcode.com/problems/jewels-and-stones/"
+"""
+
+"""
+Approach : Create a dictionary of charcaters of 'S'.Then iterate through each character of string 'J' and
+            get the value of that particular character and add it to count and finally return the count.
+"""
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        d = dict()
+        for ch in S:
+            if ch in d.keys():
+                d[ch] += 1
+            else:
+                d[ch] = 1
+        count = 0
+
+        for ch in J:
+            if ch in d.keys():
+                count += d[ch]
+        return count
+
+
+obj = Solution()
+J = "aA"
+S = "aAAbbbb"
+print(obj.numJewelsInStones(J,S))
