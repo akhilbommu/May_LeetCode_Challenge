812be13c2b2726fbb140fb73ffb2942863ac9543
akhilbommu <akhilbommu14399@gmail.com>
2020-05-13 23:04:24 +0530

    Day-13 Solution

---
+++ Day13-RemoveKDigits.py
@@ -0,0 +1,24 @@
+"""
+Problem Link : "https://leetcode.com/problems/remove-k-digits/"
+"""
+
+class RemoveKDigits:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) == k:
+            return "0"
+        stack = []
+        remove_count = k
+        for curr_ele in num:
+            while remove_count and stack and stack[-1] > curr_ele:
+                stack.pop()
+                remove_count -= 1
+            stack.append(curr_ele)
+        res = str(int("".join(stack[:len(num) - k])))
+        return res
+
+obj = RemoveKDigits()
+print(obj.removeKdigits("1432219", 3))
+print(obj.removeKdigits("10200", 1))
+print(obj.removeKdigits("10", 2))
+print(obj.removeKdigits("1234567890", 9))
+print(obj.removeKdigits("112", 1))
