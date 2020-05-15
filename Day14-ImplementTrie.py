6400e1119d52908b34b3bfe6985f94054672bc6f
akhilbommu <akhilbommu14399@gmail.com>
2020-05-15 09:50:07 +0530

    Implement Trie

---
+++ Day14-ImplementTrie.py
@@ -0,0 +1,89 @@
+"""
+Problem Link : "https://leetcode.com/problems/implement-trie-prefix-tree/"
+"""
+
+"""
+# Using Lists 
+class Trie:
+
+    def __init__(self):
+        self.trie = []
+
+    def insert(self, word: str) -> None:
+        self.trie.append(word)
+
+    def search(self, word: str) -> bool:
+        if word in self.trie:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        for word in self.trie:
+            if word.startswith(prefix):
+                return True
+        return False
+
+"""
+
+
+# Using Tries
+
+
+class TreeNode:
+
+    def __init__(self, v):
+        self.val = v
+        self.children = {}
+        self.endhere = False
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TreeNode(None)
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        parent = self.root
+        for i, char in enumerate(word):
+            if char not in parent.children:
+                parent.children[char] = TreeNode(char)
+            parent = parent.children[char]
+            if i == len(word) - 1:
+                parent.endhere = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        parent = self.root
+        for char in word:
+            if char not in parent.children:
+                return False
+            parent = parent.children[char]
+        return parent.endhere
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        parent = self.root
+        for char in prefix:
+            if char not in parent.children:
+                return False
+            parent = parent.children[char]
+        return True
+
+
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+print(trie.search("app"))
+print(trie.startsWith("app"))
+trie.insert("app")
+print(trie.search("app"))
