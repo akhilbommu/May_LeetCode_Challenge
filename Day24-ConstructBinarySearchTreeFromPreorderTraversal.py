98ba196e29b6500a00611b1f50c1ed68bafebcd3
akhilbommu <akhilbommu14399@gmail.com>
2020-05-25 11:25:02 +0530

    Day-24 Solution

--- Day24-ConstructBinarySearchTreeFromPreorderTraversal.py
+++ Day24-ConstructBinarySearchTreeFromPreorderTraversal.py
@@ -21,3 +21,39 @@ class Solution:
         root_node.left = self.bstFromPreorder(preorder[1:index])
         root_node.right = self.bstFromPreorder(preorder[index:])
         return root_node
+
+
+"""
+Java Solution : 
+--------------
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return constructBST(preorder,0,preorder.length-1);
+    }
+    public TreeNode constructBST(int[] preorder,int start,int end){
+        if(start > end){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[start]);
+        int i;
+        for(i=start;i<=end;i++){
+            if(preorder[i] > root.val){
+                break;
+            }
+        }
+        root.left = constructBST(preorder,start+1,i-1);
+        root.right = constructBST(preorder,i,end);
+        return root;
+    }
+}
+"""
